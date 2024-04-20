fec6cdc2d9ba1525911eecf499fdc59a6f6e5902
jordan <jordan@netconnect.co.zw>
2024-04-20 20:23:09 +0200

    Entry Page of project 1 working

--- web50/projects/2020/x/wiki/encyclopedia/views.py
+++ web50/projects/2020/x/wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
 
 
@@ -7,4 +7,14 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def page(request, entry):
+    result = util.get_entry(entry)
+    if result:
+        result = markdown2.markdown(result)
+        return render(request, "encyclopedia/entry.html", {
+            "entry":result
+        })
+    else:
+        return render(request, "encyclopedia/error.html")
 
